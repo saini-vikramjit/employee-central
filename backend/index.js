e8b01605a9a4f874afaf3a0b01abeae0d1b881b9
Vikramjit <saini.vikramjit@gmail.com>
2018-03-06 00:49:55 +0530

    Employee REST API
    Implemented Employee CRUD operations

--- backend/index.js
+++ backend/index.js
@@ -8,39 +8,83 @@ const app = express();
 const port = 4000;
 const hostname = '127.0.0.1';
 
+// Connect to Mongoose
 const url = 'mongodb://localhost:27017/employee-central';
+mongoose.connect(url, function (err, db) {
+    if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+    console.log('Connection established to', url);
+    }
+});
+
+app.use(bodyParser.json());
 
 /*const loginedIn = function(req, res, next){
     console.log("Logged In");
     return next();
 };*/
 
-const employees = {
+/*const employees = {
     first_name: 'John',
     last_name: 'Deo',
     age: 30
-};
+};*/
 
 app.get('/', function(req,res){
-    //res.json(employees);
-    Employee.find({}).then(rec => {
-        //res.json(eachOne);
-        console.log(rec);
-    })
     res.send('Hello');
 });
 
-app.listen(port,hostname,function(){
-    
-    //====MONGOOSE CONNECT===//
-    mongoose.connect(url, function (err, db) {
-        if (err) {
-        console.log('Unable to connect to the mongoDB server. Error:', err);
-        } else {
-        console.log('Connection established to', url);
-        }
-    });
-    //==========================//
+// Get Employees list
+app.get('/employee/list', function(req,res){
+    Employee.getEmployees((err, employees) => {
+		if(err){
+			throw err;
+		}
+		res.json(employees);
+	});
+});
 
+
+// Add Employee
+app.post('/employee/add', function(req, res){
+    let employee = req.body;
+    Employee.addEmployee(employee, (err, employee) => {
+        if(err){
+            throw err;
+        }
+        res.json(employee);
+    });
+});
+
+
+// Update Employee
+app.put('/employee/update/:_id', function(req, res){
+    let employeeId = req.params._id,
+        employee = req.body;
+    Employee.updateEmployee(employeeId,employee, (err, employee) => {
+        if(err){
+            throw err;
+        }
+        res.json(employee);
+    });
+});
+
+
+// Delete Employee
+app.delete('/employee/delete/:_id', function(req, res){
+    let employeeId = req.params._id;
+    Employee.removeEmployee(employeeId, (err, employee) => {
+        if(err){
+            throw err;
+        }
+        res.json(employee);
+    });
+})
+
+
+app.listen(port,hostname,function(){
     console.log(`Listening on the port ${port}`);
 });
+
+
--- backend/models/employeeSchema.js
+++ backend/models/employeeSchema.js
@@ -5,20 +5,77 @@ let Schema = mongoose.Schema;
 const employeeSchema = new Schema({
     first_name: {
         type: mongoose.Schema.Types.String,
-        required: true,
+        required: true
     } ,
     last_name: {
         type: mongoose.Schema.Types.String,
-        required: true,
+        required: true
     },
     age: {
         type: mongoose.Schema.Types.Number,
-        required: true,
+        required: false
     },
     gender: {
         type: mongoose.Schema.Types.String,
-        required: true,
+        required: false
     },
-})
+    email: {
+        type: mongoose.Schema.Types.String,
+        required: true
+    },
+    address:{
+        street: {
+            type: mongoose.Schema.Types.String,
+            required: false
+        },
+        city: {
+            type: mongoose.Schema.Types.String,
+            required: false
+        },
+        state: {
+            type: mongoose.Schema.Types.String,
+            required: false
+        }
+    },
+    phone: {
+        type: mongoose.Schema.Types.Number,
+        required: false
+    },
+    created_date: { 
+        type: Date, 
+        default: Date.now 
+    }
+});
+
+
 const Employee = mongoose.model('employees', employeeSchema);
-module.exports = Employee;
+
+
+// Get Employees
+module.exports.getEmployees = (callback) => {
+    Employee.find(callback);
+}
+
+
+// Add Employee
+module.exports.addEmployee = (employee, callback) => {
+	Employee.create(employee, callback);
+}
+
+
+// Update Employee
+module.exports.updateEmployee = (id, employee, options, callback) => {
+	var query = {_id: id};
+	var update = {
+        email: employee.email,
+        phone: employee.phone 
+	}
+	Employee.findOneAndUpdate(query, update, options, callback);
+}
+
+
+// Delete Employee
+module.exports.removeEmployee = (id, callback) => {
+	var query = {_id: id};
+	Employee.remove(query, callback);
+}
